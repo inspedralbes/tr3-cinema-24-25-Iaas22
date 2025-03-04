36d40a72bec44a10f2eb3680d3ba5bc6080e2265
Iaas22 <ishaaamin22@gmail.com>
2025-03-04 17:20:27 +0100

    elimina pelicula correctamente

--- back/app/Http/Controllers/MovieController.php
+++ back/app/Http/Controllers/MovieController.php
@@ -48,4 +48,49 @@ class MovieController extends Controller
             'movie' => $movie
         ], 201);
     }
+    // Editar una película en la base de datos
+public function update(Request $request, $id)
+{
+    // Buscar la película por ID
+    $movie = Movie::find($id);
+
+    if (!$movie) {
+        return response()->json(['error' => 'Película no encontrada'], 404);
+    }
+
+    // Validar los datos
+    $request->validate([
+        'title' => 'sometimes|string|max:255',
+        'duration' => 'sometimes|integer',
+        'genre' => 'sometimes|string|max:255',
+        'director' => 'sometimes|string',
+        'actors' => 'sometimes|string',
+        'sinopsis' => 'sometimes|string',
+        'premiere' => 'sometimes|date',
+        'room_id' => 'sometimes|exists:rooms,id'
+    ]);
+
+    // Actualizar los campos que se han enviado
+    $movie->update($request->all());
+
+    return response()->json([
+        'message' => 'Película actualizada con éxito',
+        'movie' => $movie
+    ], 200);
+}
+
+// Eliminar una película de la base de datos
+public function destroy($id)
+{
+    $movie = Movie::find($id);
+
+    if (!$movie) {
+        return response()->json(['error' => 'Película no encontrada'], 404);
+    }
+
+    $movie->delete();
+
+    return response()->json(['message' => 'Película eliminada con éxito'], 200);
+}
+
 }
--- back/routes/api.php
+++ back/routes/api.php
@@ -4,14 +4,11 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MovieController;
 
-
-
 Route::get('/movies', [MovieController::class, 'index']);  // Obtener todas las películas
 Route::get('/movies/{id}', [MovieController::class, 'show']); // Obtener película por ID
 Route::post('/movies', [MovieController::class, 'store']);  // Crear una nueva película
-
-
-
+Route::put('/movies/{id}', [MovieController::class, 'update']); // Actualizar una película
+Route::delete('/movies/{id}', [MovieController::class, 'destroy']); // Eliminar una película
 
 Route::get('/user', function (Request $request) {
     return $request->user();
